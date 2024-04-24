1452bdce836830a64f62bfa6e5141cafd7aeeeaa
marmartinez96 <manuelagustinmartinez@hotmail.com>
2024-04-24 16:43:59 -0300

    Migracion de base de datos y .gitignore actualizado

---
+++ ProdigyPlanningAPI/Migrations/20240424194104_24042024.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProdigyPlanningAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class _24042024 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__categori__3213E83FC6E8B697", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    surname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    email = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    password = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    roles = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__users__3213E83F54C06594", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "events",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    date = table.Column<DateTime>(type: "datetime", nullable: true),
+                    location = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    created_by = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__events__3213E83F0186EDBA", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__events__created___70DDC3D8",
+                        column: x => x.created_by,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "category_events",
+                columns: table => new
+                {
+                    category_id = table.Column<int>(type: "int", nullable: false),
+                    event_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__category__B779E6C6F9A4F5CC", x => new { x.category_id, x.event_id });
+                    table.ForeignKey(
+                        name: "FK__category___categ__6EF57B66",
+                        column: x => x.category_id,
+                        principalTable: "categories",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK__category___event__6FE99F9F",
+                        column: x => x.event_id,
+                        principalTable: "events",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_category_events_event_id",
+                table: "category_events",
+                column: "event_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_events_created_by",
+                table: "events",
+                column: "created_by");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "category_events");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+
+            migrationBuilder.DropTable(
+                name: "events");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
