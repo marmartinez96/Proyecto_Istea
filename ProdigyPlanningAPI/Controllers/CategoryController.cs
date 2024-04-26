146f0a13e6673464bbdca29279783f1d0333e3f1
Manuel Martinez <manuel.martinez@presidencia.gob.ar>
2024-04-26 20:46:05 -0300

    Se emepzo la implementacion del ABM de categorias

--- ProdigyPlanningAPI/Controllers/CategoryController.cs
+++ ProdigyPlanningAPI/Controllers/CategoryController.cs
@@ -5,13 +5,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json.Linq;
 using ProdigyPlanningAPI.Data;
+using ProdigyPlanningAPI.FormModels;
 using ProdigyPlanningAPI.Helpers;
 using ProdigyPlanningAPI.Models;
 using System.Security.Claims;
 
 namespace ProdigyPlanningAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class CategoryController : ControllerBase
     {
@@ -21,31 +22,164 @@ namespace ProdigyPlanningAPI.Controllers
             _context = context;
         }
 
-        [Authorize]
         [HttpGet]
         public dynamic GetCategories() 
         {
+            bool success = true;
+            string message = "Success";
+            List<Category> result = null;
+            try
+            {
+                result = _context.Categories.ToList();
+            }
+            catch (Exception e)
+            {
+                success = false;
+                message = e.Message;
+            }
+
+            return new
+            {
+                success = success,
+                message = message,
+                result = result
+            };
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("Add")]
+        public dynamic AddCategory(Category category) 
+        {
+            bool success = true;
+            string message = "";
             var identity = HttpContext.User.Identity as ClaimsIdentity;
             var token = AuthorizationHelper.ValidateToken(identity, _context);
             if (!token.success) return token;
 
             User user = token.result;
 
-            if (user.Roles != "[ROLE_ORGANIZER]")
+            if (user.Roles != "[ROLE_ADMIN]")
             {
                 return new
                 {
                     success = false,
-                    message = "No tiene permisos para ver este recurso",
-                    result = ""
+                    message = "Necesita permisos de administrador para utilizar este recurso",
                 };
+            }
+
+            try
+            { 
+                Category _category = new Category();
+                _category.Name = category.Name;
+                _context.Categories.Add(_category);
+                _context.SaveChanges();
+                message = "Se ha creado la categoria " + _category.Name;
+            }
+            catch (Exception e)
+            {
+                success= false;
+                message = e.Message;
+
             }
 
             return new
             {
-                success = true,
-                message = "",
-                result = _context.Categories.ToList()
+                success = success,
+                message = message,
+            };
+        }
+
+        [Authorize]
+        [HttpPatch]
+        [Route("Edit")]
+        public dynamic EditCategory(ChangeCategoryModel changeCategoryModel)
+        {
+            bool success = true;
+            string message = "";
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            var token = AuthorizationHelper.ValidateToken(identity, _context);
+            if (!token.success) return token;
+
+            User user = token.result;
+
+            if (user.Roles != "[ROLE_ADMIN]")
+            {
+                return new
+                {
+                    success = false,
+                    message = "Necesita permisos de administrador para utilizar este recurso",
+                };
+            }
+
+            try
+            {
+                Category _category = _context.Categories.FirstOrDefault(c => c.Name == changeCategoryModel.OldName);
+                if (_category == null) 
+                {
+                    throw new Exception("La categoria que desea modificar no existe");
+                }
+                if (changeCategoryModel.NewName == _category.Name)
+                {
+                    throw new Exception("El nuevo nombre coincide con el anterior");
+                }
+                _category.Name = changeCategoryModel.NewName;
+                _context.SaveChanges();
+                message = "Se ha actualizado el nombre de la categoria " + changeCategoryModel.OldName + " a: " + _category.Name;
+            }
+            catch (Exception e)
+            {
+                success = false;
+                message = e.Message;
+            }
+            return new
+            {
+                success = success,
+                message = message,
+            };
+        }
+
+        [Authorize]
+        [HttpDelete]
+        [Route("Delete")]
+        public dynamic DeleteCategory(Category category)
+        {
+            bool success = true;
+            string message = "";
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            var token = AuthorizationHelper.ValidateToken(identity, _context);
+            if (!token.success) return token;
+
+            User user = token.result;
+
+            if (user.Roles != "[ROLE_ADMIN]")
+            {
+                return new
+                {
+                    success = false,
+                    message = "Necesita permisos de administrador para utilizar este recurso",
+                };
+            }
+            try
+            {
+                Category _category = _context.Categories.FirstOrDefault(c => c.Name == category.Name);
+                if (_category == null)
+                {
+                    throw new Exception("La categoria que desea eliminar no existe");
+                }
+                _context.Categories.Remove(_category);
+                _context.SaveChanges();
+                message = "Se ha eliminado la categoria " + category.Name;
+            }
+            catch (Exception e)
+            {
+                success = false;
+                message = e.Message;
+            }
+            return new
+            {
+                success = success,
+                message = message,
             };
         }
     }
