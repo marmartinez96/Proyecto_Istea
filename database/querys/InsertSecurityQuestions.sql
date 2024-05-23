f3c4068d0b07f2184b6654d9b4286f0b1e2b2dc2
marmartinez96 <manuelagustinmartinez@hotmail.com>
2024-05-22 22:17:12 -0300

    Create InsertSecurityQuestions.sql

---
+++ database/querys/InsertSecurityQuestions.sql
@@ -0,0 +1,14 @@
+USE [ProdigyPlanning]
+GO
+
+INSERT INTO [dbo].[security_questions]
+           ([question])
+     VALUES ('¿Cuál era el apodo de tu mejor amigo de la infancia?'),
+	 ('¿Cuál era el apodo de tu mejor amigo de la infancia?'),
+	 ('¿En qué ciudad se conocieron sus padres?'),
+	 ('¿Cuál es el apellido de tu vecino?'),
+	 ('¿Cuántas mascotas tenías a los 10 años?'),
+	 ('¿En qué mes te casaste?');
+
+GO
+
