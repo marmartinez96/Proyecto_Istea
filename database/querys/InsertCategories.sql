df1da118b4819e76bbc405a9afb25a204ccb05fc
marmartinez96 <manuelagustinmartinez@hotmail.com>
2024-05-24 20:45:17 -0300

    Create InsertCategories.sql

---
+++ database/querys/InsertCategories.sql
@@ -0,0 +1,15 @@
+USE [ProdigyPlanning]
+GO
+
+INSERT INTO [dbo].[categories]
+           ([name])
+     VALUES ('Deportes'),
+	 ('Charlas'),
+	 ('Música'),
+	 ('Tecnología'),
+	 ('Gastronomía'),
+	 ('Entretenimiento')
+	 ('Otros');
+
+GO
+
